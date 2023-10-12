a34494431126e829011d018845eb37a83eaa538f
PaulaRejon91 <paulateresamaria@gmail.com>
2023-10-12 13:41:45 +0200

    Añado lógica para registar usuario con e-mail. El usuario se guarda en la bbdd de firebase correctamente.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,19 @@ const auth = getAuth(appFirebase)
 const Login = () => {
 
     const [register, setRegister] = useState(false) //empieza en falso
-    
+
+    const funAuthentication = async(e) =>{
+        e.preventDefault() //duda: ver tutotial 6, minuto 3.
+        const email= e.target.email.value;
+        const password=e.target.password.value;
+
+        if(register){
+            await createUserWithEmailAndPassword(auth,email,password) //await porque es una operación asíncrona
+        }
+            else{
+                await signInWithEmailAndPassword(auth,email,password)
+            }
+    }
 
     return (
 
@@ -21,11 +33,12 @@ const Login = () => {
                     <div className="dad">
                         <div className="card card-body shadow-lg">
                             <img src={Imagen1} alt="" className='style-profile' />
-                            <form>
-                                <input type="text" placeholder='Enter your email here' className='text-box' />
-                                <input type="password" placeholder='Enter your password here' className='text-box' />
+                            <form onSubmit ={funAuthentication}>    {/*//duda on submit: ver tutotial 6, minuto 3.*/}
+                                <input type="text" placeholder='Enter your email here' className='text-box'id='email' />
+                                <input type="password" placeholder='Enter your password here' className='text-box' id='password'/>
                                 <button className='btnform'>{register ? "Register" : "Login"}</button>
-                            </form>
+                            </form > 
+                            
                             <h4 className='text'>{register ? "If you already have an account" : "if you don't have an account"} <button className='btnswicth' onClick={() => setRegister(!register)}>{register ? "Login" : "Register"}</button></h4>
                         </div>
                     </div>
